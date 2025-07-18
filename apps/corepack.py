5f47acb27e30ba4b6ebea463fecdb8c0dc85eadf
Jérôme Vasseur <jerome.vasseur@jhome.fr>
2025-07-18 13:00:01 +0200

    Add missing npm and npx commands to corepack

--- apps/corepack.py
+++ apps/corepack.py
@@ -1,9 +1,19 @@
-from jvasseur.packaging.app.npm import NpmApp
+from jvasseur.packaging.app.npm import NpmApp, create_node_command
 from config import base_url
 
 class Corepack(NpmApp):
     uri = base_url + 'corepack.xml'
     name = 'corepack'
+
+    def commands(self, data):
+        constraint = data.get('engines', {}).get('node')
+
+        # npm and npx are not defined in version data (probably to prevent conflicts) so we have to add them manualy
+        commands = super().commands(data)
+        commands.append(create_node_command('npm', './dist/npm.js', constraint))
+        commands.append(create_node_command('npx', './dist/npx.js', constraint))
+
+        return commands
 
 if __name__ == '__main__':
     Corepack().main()

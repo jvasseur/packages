1e2211f5821edf17d61f04980a79688f237642e0
Jérôme Vasseur <jerome.vasseur@jhome.fr>
2025-07-18 11:32:35 +0200

    Initialize repository

---
+++ bin/build.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+cd $(dirname $(cd $(dirname $0) && pwd))
+
+BASE_URL=$(python3 -c "import config; print(config.base_url)")
+SIGNING_KEY=$(python3 -c "import config; print(config.signing_key)")
+
+LAST_MESSAGE=$(cd public && git log -1 --pretty=%B)
+LAST_BUILT_COMMIT=${LAST_MESSAGE#"Update to "}
+
+CURRENT_COMMIT=$(git rev-parse HEAD)
+
+CHANGED_FEEDS=$(git diff --name-only $LAST_BUILT_COMMIT $CURRENT_COMMIT -- feeds)
+
+if [ -z "$CHANGED_FEEDS" ]; then
+    exit 0
+fi
+
+for FILE in $CHANGED_FEEDS; do
+    NAME=${FILE#"feeds/"}
+    NAME=${NAME%".xml"}
+
+    DEST="public/$NAME.xml"
+
+    cat "$FILE" \
+        | sed '1a<?xml-stylesheet href="resources/feed.xsl" type="text/xsl"?>' \
+        > "$DEST"
+
+    SIGNATURE=$(gpg --default-key=$SIGNING_KEY --detach-sign < $DEST | base64)
+
+    echo "<!-- Base64 Signature" >> $DEST
+    echo "$SIGNATURE" >> $DEST
+    echo "-->" >> $DEST
+
+    (cd public && git add "$NAME.xml")
+done
+
+(cd public && git commit -m "Update to $CURRENT_COMMIT")
---
+++ config.py
@@ -0,0 +1,3 @@
+base_url = 'https://packages.jvasseur.me/'
+
+signing_key = '0x7FE27C0710C58CCC2F25928275D1F73CBBC9AF88'

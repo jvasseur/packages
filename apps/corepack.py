1647070c23f45dde78f60257874a2cc538281106
Jérôme Vasseur <jerome.vasseur@jhome.fr>
2025-07-18 11:57:27 +0200

    Add missing interface uri

--- apps/corepack.py
+++ apps/corepack.py
@@ -1,6 +1,8 @@
-import jvasseur.packaging.app.npm import NpmApp
+from jvasseur.packaging.app.npm import NpmApp
+from config import base_url
 
 class Corepack(NpmApp):
+    uri = base_url + 'corepack.xml'
     name = 'corepack'
 
 if __name__ == '__main__':
